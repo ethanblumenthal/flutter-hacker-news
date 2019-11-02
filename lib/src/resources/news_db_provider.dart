8aed42c2a3accb5f099fc6cd47262d65402a227b
ethanblumenthal <ethan.blumenthal@gmail.com>
2019-11-01 20:02:19 -0500

    Refactored respository to use abstract classes.

--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -1,12 +1,22 @@
+import 'package:news/src/resources/news_api_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 import 'package:path/path.dart';
 import 'dart:async';
 import '../models/item_model.dart';
+import 'repository.dart';
 
-class NewsDbProvider {
+class NewsDbProvider implements Source, Cache {
   Database db;
+
+  NewsDbProvider() {
+    init();
+  }
+
+  Future<List<int>> fetchTopIds() {
+    return null;
+  }
 
   void init() async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
@@ -56,3 +66,5 @@ class NewsDbProvider {
     return db.insert('Items', item.toMap());
   }
 }
+
+final newsDbProvider = NewsDbProvider();
