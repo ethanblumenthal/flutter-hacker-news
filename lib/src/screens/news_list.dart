63d56c0245260c073ac66c42cc24bcdfd013f344
ethanblumenthal <ethan.blumenthal@gmail.com>
2019-11-03 10:49:43 -0600

    Implemented swipe to refresh to clear cache.

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:news/src/blocs/stories_bloc.dart';
 import '../blocs/stories_provider.dart';
 import '../widgets/news_list_tile.dart';
+import '../widgets/refresh.dart';
 
 class NewsList extends StatelessWidget {
   Widget build(context) {
@@ -27,13 +28,15 @@ class NewsList extends StatelessWidget {
           );
         }
 
-        return ListView.builder(
-          itemCount: snapshot.data.length,
-          itemBuilder: (context, int index) {
-            return NewsListTile(
-              itemId: snapshot.data[index],
-            );
-          },
+        return Refresh(
+          child: ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, int index) {
+              return NewsListTile(
+                itemId: snapshot.data[index],
+              );
+            },
+          ),
         );
       },
     );
